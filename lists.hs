2eecf8ba4b072361ae409b1a57e287544bc995e4
oss6 <ossedb@gmail.com>
2015-10-04 01:39:48 +0100

    Added more fn

--- lists.hs
+++ lists.hs
@@ -4,14 +4,35 @@
 
 
 -- Function: mLast
-mLast [] 	 = error "Empty list"
-mLast [x] 	 = x
+mLast [] = error "Empty list"
+mLast [x] = x
 mLast (_:xs) = mLast xs
 
 -- Function: mFirst
-mFirst [] 	  = error "Empty list"
-mFirst (x:_)  = x
+mFirst [] = error "Empty list"
+mFirst (x:_) = x
 
 -- Function: mLength
 mLength [] = 0
 mLength (_:xs) = 1 + mLength xs
+
+-- Function: elementAt
+elementAt [] _ = error "Index out of bounds"
+elementAt (x : _) 0 = x
+elementAt (x : xs) n = elementAt xs (n - 1)
+
+-- Function: mReverse
+mReverse [] = []
+mReverse (x : xs) = (mReverse xs) ++ [x]
+
+-- Function: isPalindrome
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome l = l == reverse l
+
+-- Function: flatten
+flatten :: [[a]] -> [a]
+flatten l = flatten' l []
+        where
+        flatten' [] acc = acc
+        flatten' (x : xs) acc = flatten' xs (acc ++ x)
+
