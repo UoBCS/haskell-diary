c5ada7eb32bc6486e92b3df28af5fdb43345ad45
oss6 <ossedb@gmail.com>
2015-11-16 14:04:15 +0000

    Added function applicative

--- functors/applicatives.hs
+++ functors/applicatives.hs
@@ -4,7 +4,24 @@ class (Functor f) => Applicative' f where
       pure :: a -> f a
       (<*>) :: f (a -> b) -> f a -> f b
 
-instance Applicative Maybe where
+instance Applicative' Maybe where
          pure          = Just
          Nothing <*> _ = Nothing
          Just f <*> a  = fmap f a
+
+
+{-instance Applicative' IO where
+         pure    = return
+         a <*> b = do
+                   f <- a
+                   x <- b
+                   return (f x)-}
+
+
+instance Applicative' IO where
+         pure    = return
+         a <*> b = a >>= (\f -> b >>= (\x -> return (f x)))
+
+instance Applicative' ((->) r) where
+         pure a   = \_ -> a
+         ff <*> g = (\x -> ff x (g x))
